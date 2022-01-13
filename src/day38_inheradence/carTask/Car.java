19640aa1725c39a3ce422949142781c7d08d69cd
Dimo <dvelikov24@gmail.com>
2022-01-13 10:23:17 -0500

    Car class constructor added

--- src/day38_inheradence/carTask/Car.java
+++ src/day38_inheradence/carTask/Car.java
@@ -7,5 +7,12 @@ public class Car {
     public String color;
     public int miles;
 
-
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
 }
