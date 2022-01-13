a37f4d8e8a85d61d8013f03b1f3c045ccd949220
Dimo <dvelikov24@gmail.com>
2022-01-13 11:01:30 -0500

    BMW class completed

--- src/day38_inheradence/carTask/BMW.java
+++ src/day38_inheradence/carTask/BMW.java
@@ -1,4 +1,15 @@
 package day38_inheradence.carTask;
 
-public class BMW {
+public class BMW extends Car{
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breakDown(){
+        System.out.println(brand+" "+ model+ " break down every 100 miles");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+ model+ " is racing");
+    }
 }
