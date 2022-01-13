1339e55eb2705133f6ac233b6235b255dcfb167a
Dimo <dvelikov24@gmail.com>
2022-01-13 10:51:51 -0500

    Toyota class instance method added

--- src/day38_inheradence/carTask/Toyota.java
+++ src/day38_inheradence/carTask/Toyota.java
@@ -4,4 +4,7 @@ public class Toyota extends Car {
     public Toyota( String model, int year, double price, String color, int miles) {
         super("Toyota", model, year, price, color, miles);
     }
+    public void  reliable(){
+        System.out.println(brand+ " "+ model+" is reliable");
+    }
 }
